a5fdf5cf0be3572a3768f97a1bf078368d319b0c
zjc123456 <2233737325@qq.com>
2021-10-09 17:35:19 +0800

    fix:iconfont丢失

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import xixixiui from '../packages/index.js'
-import './assets/fonts/iconfont.js'
+import router from './router'
 import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
-Vue.use(xixixiui)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
